13cb87bac24d7119f66a44e4ab45317390ec82e9
EdgarOrtiz10 <2120eaoe@gmail.com>
2022-08-07 15:32:24 -0500

    Services - Models (Clientes & Addcliente)

---
+++ src/app/features/addcliente/models/addcliente.ts
@@ -0,0 +1,11 @@
+export interface Addcliente {
+  idCliente: number;
+  nombre1: string;
+  nombre2: string;
+  apellido1: string;
+  apellido2: string;
+  direccion: string;
+  telefono: number;
+  sexo: string;
+  correo: string;
+}
