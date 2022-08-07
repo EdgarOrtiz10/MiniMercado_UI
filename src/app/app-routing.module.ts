00d360a1d89d41ceba3fb6f3d49aa3c93e94960d
EdgarOrtiz10 <2120eaoe@gmail.com>
2022-08-07 10:10:03 -0500

    RouterLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddclienteComponent } from './features/addcliente/addcliente.component';
+import { HomeComponent } from './features/home/home.component';
 
 const routes: Routes = [
+  {
+    path:'Home',
+    component: HomeComponent
+  },
+  {
+    path:'AddCliente',
+    component: AddclienteComponent
+  },
   {
     path:'**',
     redirectTo: ''
